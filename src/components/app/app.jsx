959eea035e137eaac2beb08b515c0d5c66a6c171
dzaitseva <dzaitseva@phoenixit.ru>
2019-10-23 11:45:35 +0300

    small fix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,11 +4,11 @@ import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 export const App = () => {
   const GameSettings = {
     TIME: 5,
-    ERROR_COUNTS: 3,
+    ERROR_COUNT: 3
   };
   return <WelcomeScreen
     time={GameSettings.TIME}
-    errorCount={GameSettings.ERROR_COUNTS}
+    errorCount={GameSettings.ERROR_COUNT}
   />;
 };
 
